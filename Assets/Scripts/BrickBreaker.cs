943890f514c319f195cfed102c86695cf065ac65
MightyK1997 <31252492+MightyK1997@users.noreply.github.com>
2017-11-07 13:02:46 -0700

    FirstPush

---
+++ Assets/Scripts/BrickBreaker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickBreaker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public GameObject particle;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount >0)
+        {
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                if (Input.GetTouch(i).phase == TouchPhase.Began)
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+                    if (Physics.Raycast(ray))
+                    {
+                        Destroy(particle);
+                    }
+                }
+            }
+        }
+	}
+}

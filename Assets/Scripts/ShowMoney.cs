79ca4c011c773cd5bfb0ae823b13873048a7ebe2
MightyK1997 <31252492+MightyK1997@users.noreply.github.com>
2017-11-15 21:35:53 -0700

    new working build

--- Assets/Scripts/ShowMoney.cs
+++ Assets/Scripts/ShowMoney.cs
@@ -6,15 +6,17 @@ using UnityEngine.UI;
 public class ShowMoney : MonoBehaviour {
 
     public Text moneyText;
+    public Text moneyText2;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         moneyText.text = CollectionAndMonster.money.ToString();
-		
-	}
+        moneyText2.text = CollectionAndMonster.money.ToString();
+
+    }
 }
